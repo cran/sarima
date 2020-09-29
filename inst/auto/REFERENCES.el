5059f57a602a2dcad9fa9b3f0f665957c8b9efd7
Georgi N. Boshnakov <georgi.boshnakov@manchester.ac.uk>
2020-09-29 09:00:14 +0000

    version 0.8.4

---
+++ inst/auto/REFERENCES.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "REFERENCES"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "BrockwellDavis1991"
+    "FrancqZakoian2010garch"
+    "Li2004diagnostic"
+    "McLeodYuKrougly2007"
+    "kokoszka2011nonlinearity"
+    "gardner+harvey+phillips:1980"))
+ :bibtex)
+
